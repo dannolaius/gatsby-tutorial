2f6a27dd0d5c77265bb96ffa3e840f7270d9e031
Daniel Lai Wikström <daniellw@kth.se>
2024-10-15 03:41:47 +0200

    hi

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,11 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
+  pathPrefix: '/gatsby-tutorial',
   siteMetadata: {
     title: `My Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
-    pathPrefix: '/gatsby-tutorial'
+    
   },
   plugins: [],
 }
